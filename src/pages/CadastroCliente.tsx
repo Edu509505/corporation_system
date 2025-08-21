92b991ef69d1c6dc39e592ce5d4ab5b9a92f4e63
Eduardo Nascimento <eduardonascimento60650@gmail.com>
2025-08-21 00:33:14 -0300

    Estilizando e colocando novas funções
    Foi adicionado imagens e nova estilização ao projeto, além disso, foi adicionado mais um componente, desta vez coloquei o Alert da bilioteca ui.shadcn.com a função desse Alert é dizer ao Usuário se o cliente foi cadastrado ou não

--- src/pages/CadastroCliente.tsx
+++ src/pages/CadastroCliente.tsx
@@ -1,117 +1,189 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { CircleCheck, CircleFadingPlusIcon } from "lucide-react";
 import { useState } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 //import type { Cliente } from "../Tipagens"
 
 interface Cliente {
-    cliente: string;
-    cnpj: string;
-    local: string;
-    status: "Ativo" | "Pendente" | "Inativo" | null;
-    path: string | null;
+  cliente: string;
+  cnpj: string;
+  local: string;
+  status: "Ativo" | "Pendente" | "Inativo" | null;
+  path: string | null;
 }
 
-
 const url = import.meta.env.VITE_API_URL;
-//Qualquer Link relacionado ao Back-End sempre importar o .env como boa prática 
+//Qualquer Link relacionado ao Back-End sempre importar o .env como boa prática
 
 export default function CriarCliente() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const voltar = () => {
+    navigate(-1);
+  };
 
-    const voltar = () => {
-        navigate(-1)
-    }
+  const [novoCliente, setNovoCliente] = useState<Cliente>({
+    cliente: "",
+    cnpj: "",
+    local: "",
+    status: "Ativo",
+    path: "",
+  });
 
-    const [novoCliente, setNovoCliente] = useState<Cliente>({
-        cliente: '',
-        cnpj: '',
-        local: '',
-        status: 'Ativo',
-        path: '',
+  console.log(novoCliente);
+
+  async function criarCliente(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const response = await fetch(`${url}/clientes`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        cliente: novoCliente.cliente,
+        cnpj: novoCliente.cnpj,
+        local: novoCliente.local,
+        status: novoCliente.status,
+        path: novoCliente.path,
+      }),
     });
+    const body = await response.json();
+  }
 
-    console.log(novoCliente)
+  const mensagemDeOkay = "Okay";
 
-    async function criarCliente(event: React.FormEvent<HTMLFormElement>) {
-        event.preventDefault();
-        const response = await fetch(`${url}/clientes`, {
-            method: "POST",
-            headers: {
-                "Content-type": "application/json"
-            },
-            body: JSON.stringify({
-                cliente: novoCliente.cliente,
-                cnpj: novoCliente.cnpj,
-                local: novoCliente.local,
-                status: novoCliente.status,
-                path: novoCliente.path,
-            })
-        });
-        const body = await response.json();
-    }
-    return (
-        <div className="bg-gray-50">
-            <form onSubmit={criarCliente} className="flex gap-3 flex-col p-5">
-                <h1>Cadastrar Cliente</h1>
+  const mensagemAlert = [
+    {
+      submitOkay: {
+        alertDialogTitle: "Cliente Cadastrado!",
+        alertDialogDescription: "Cliente foi cadastrado com sucesso!",
+      },
+      submitError: {
+        alertDialogTitle: "Erro ao cadastrar usuário",
+        alertDialogDescription:
+          "Você precisa preencher todos os campos para que o cadastro seja realizado",
+      },
+    },
+  ];
 
-                <Input
-                    type="text"
-                    name="cliente"
-                    placeholder="Nome do cliente"
-                    value={novoCliente.cliente}
-                    onChange={(event) => setNovoCliente({
-                        ...novoCliente,
-                        cliente: event.target.value
-                    })}
-                    className="bg-white"
-                />
-                <Input
-                    type="text"
-                    name="cnpj"
-                    placeholder="Cnpj do cliente"
-                    value={novoCliente.cnpj}
-                    onChange={(event) => setNovoCliente({
-                        ...novoCliente,
-                        cnpj: event.target.value
-                    })}
-                    className="bg-white"
-                />
-                <Input
-                    type="text"
-                    name="local"
-                    placeholder="local do cliente"
-                    value={novoCliente.local}
-                    onChange={(event) => setNovoCliente({
-                        ...novoCliente,
-                        local: event.target.value
-                    })}
-                    className="bg-white"
-                />
-                <select
-                    name="select"
-                    onChange={(event) => setNovoCliente({
-                        ...novoCliente,
-                        status: event.target.value as "Ativo" | "Pendente" | "Inativo"
-                    })}
+  return (
+    <div className="w-full h-screen flex flex-col bg-[url('./src/assets/img/IMG-20240823-WA0007.jpg')] bg-cover bg-center">
+      <div className="w-full h-full flex flex-col backdrop-blur-xl bg-[rgba(255,255,255,0.40)]">
+        <form onSubmit={criarCliente} className="flex gap-3 flex-col p-5">
+          <div className="flex gap-3 items-center">
+            <CircleFadingPlusIcon className="size-10" />
+            <h1 className="text-2xl font-bold">Cadastrar novo Cliente</h1>
+          </div>
 
-                >
-                    <option value="Ativo" >
-                        Ativo
-                    </option>
-                    <option value="Pendente" >
-                        Pendente
-                    </option>
-                    <option value="Inativo" >
-                        Inativo
-                    </option>
-                </select>
+          <Input
+            type="text"
+            name="cliente"
+            placeholder="Nome do cliente"
+            value={novoCliente.cliente}
+            onChange={(event) =>
+              setNovoCliente({
+                ...novoCliente,
+                cliente: event.target.value,
+              })
+            }
+            className="bg-white"
+          />
+          <Input
+            type="text"
+            name="cnpj"
+            placeholder="Cnpj do cliente"
+            value={novoCliente.cnpj}
+            onChange={(event) =>
+              setNovoCliente({
+                ...novoCliente,
+                cnpj: event.target.value,
+              })
+            }
+            className="bg-white"
+          />
+          <Input
+            type="text"
+            name="local"
+            placeholder="local do cliente"
+            value={novoCliente.local}
+            onChange={(event) =>
+              setNovoCliente({
+                ...novoCliente,
+                local: event.target.value,
+              })
+            }
+            className="bg-white"
+          />
+          <select
+            className="bg-white rounded-md h-8"
+            name="select"
+            onChange={(event) =>
+              setNovoCliente({
+                ...novoCliente,
+                status: event.target.value as "Ativo" | "Pendente" | "Inativo",
+              })
+            }
+          >
+            <option value="Ativo">Ativo</option>
+            <option value="Pendente">Pendente</option>
+            <option value="Inativo">Inativo</option>
+          </select>
 
-
-
-                <Button onClick={voltar} type="submit" className="cursor-pointer">Cadastrar</Button>
-            </form>
-        </div>
-    );
+          <AlertDialog>
+            <AlertDialogTrigger>
+              <Button variant="outline" className="bg-ring">
+                <CircleCheck /> Cadastrar
+              </Button>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  {mensagemAlert.map((mensagem) =>
+                    mensagemDeOkay !== "Okay" ? (
+                      <>
+                        <AlertDialogTitle>
+                          {mensagem.submitError.alertDialogTitle}
+                        </AlertDialogTitle>
+                        <AlertDialogDescription>
+                          {mensagem.submitError.alertDialogDescription}
+                        </AlertDialogDescription>
+                      </>
+                    ) : (
+                      <>
+                        <AlertDialogTitle>
+                          {mensagem.submitOkay.alertDialogTitle}
+                        </AlertDialogTitle>
+                        <AlertDialogDescription>
+                          {mensagem.submitOkay.alertDialogDescription}
+                        </AlertDialogDescription>
+                      </>
+                    )
+                  )}
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogAction
+                    type="submit"
+                    className="cursor-pointer"
+                    onClick={voltar}
+                  >
+                    Continuar
+                  </AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialogTrigger>
+          </AlertDialog>
+        </form>
+      </div>
+    </div>
+  );
 }
--- src/pages/Clientes.tsx
+++ src/pages/Clientes.tsx
@@ -1,11 +1,15 @@
 import { Button } from "@/components/ui/button";
-import { CircleAlert, CircleCheck, CirclePlusIcon, CircleX } from "lucide-react";
+import {
+  CircleAlert,
+  CircleCheck,
+  CirclePlusIcon,
+  CircleX,
+} from "lucide-react";
 import { useEffect, useState } from "react";
-import { Link } from 'react-router-dom'
-import type { Cliente } from '../Tipagens'
+import { Link } from "react-router-dom";
+import type { Cliente } from "../Tipagens";
 
 export default function VerClientes() {
-
   const url = import.meta.env.VITE_API_URL;
   //Qualquer Link relacionado ao Back-End sempre importar o .env como boa prática
 
@@ -25,13 +29,13 @@ export default function VerClientes() {
     // itensRelacionados();
   }, []);
 
-    // async function deletarCliente() {
-      
-    // }
+  // async function deletarCliente() {
+
+  // }
 
   return (
-    <>
-      <div className="w-full h-screen p-5 flex flex-col bg-gray-50">
+    <div className="w-full h-screen flex flex-col bg-[url('./src/assets/img/nossotrab2.jpg')] bg-cover bg-center">
+      <div className="w-full h-full p-5 flex flex-col backdrop-blur-xl bg-[rgba(255,255,255,0.40)]">
         <h1 className="text-2xl font-bold">Clientes cadastrados</h1>
         <div className="w-full h-20 flex justify-between items-center">
           <h2 className="text-2xl ">Lista de Clientes</h2>
@@ -61,17 +65,26 @@ export default function VerClientes() {
                 <p>
                   <strong>Local:</strong> {c.local}
                 </p>
-                  {c.status === 'Ativo' ? (<p className="flex itens-center gap-2 font-bold text-ring"><CircleCheck /> {c.status}</p>)
-                    : c.status === 'Pendente' ? (<p className="flex itens-center gap-2 font-bold text-chart-1"><CircleAlert /> {c.status}</p>)
-                      : c.status === 'Inativo' ? (<p className="flex itens-center gap-2 font-bold text-destructive"><CircleX /> {c.status}</p>)
-                        : ''
-                  }
+                {c.status === "Ativo" ? (
+                  <p className="flex itens-center gap-2 font-bold text-ring">
+                    <CircleCheck /> {c.status}
+                  </p>
+                ) : c.status === "Pendente" ? (
+                  <p className="flex itens-center gap-2 font-bold text-chart-1">
+                    <CircleAlert /> {c.status}
+                  </p>
+                ) : c.status === "Inativo" ? (
+                  <p className="flex itens-center gap-2 font-bold text-destructive">
+                    <CircleX /> {c.status}
+                  </p>
+                ) : (
+                  ""
+                )}
               </div>
-
             </div>
           ))}
         </section>
       </div>
-    </>
+    </div>
   );
 }
