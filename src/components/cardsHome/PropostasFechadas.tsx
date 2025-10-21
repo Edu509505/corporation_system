4ae3e698ed4ec7115710ed18ad1bf1497e251442
Eduardo Nascimento <eduardonascimento60650@gmail.com>
2025-10-21 17:51:12 -0300

    ***

--- src/components/cardsHome/PropostasFechadas.tsx
+++ src/components/cardsHome/PropostasFechadas.tsx
@@ -13,42 +13,37 @@ interface Proposta {
 
 function PropostasFechadas() {
 
-    const { data: propostasAprovadas } = useSuspenseQuery({
-      queryKey: ["propostasAprovadas"],
-      queryFn: async () => {
-        const response = await fetch(`${url}/propostasAprovadas`);
-        if(!response.ok) throw new Error("erro ao encontrar os versionamentos aprovados");
-        const data = await response.json();
-        return data as Proposta[];
-      }
-    })
+  const { data: propostasAprovadas } = useSuspenseQuery({
+    queryKey: ["propostasAprovadas"],
+    queryFn: async () => {
+      const response = await fetch(`${url}/propostasAprovadas`);
+      if (!response.ok) throw new Error("erro ao encontrar os versionamentos aprovados");
+      const data = await response.json();
+      return data as Proposta[];
+    }
+  })
 
-    console.log(propostasAprovadas)
+  console.log(propostasAprovadas)
 
-    return(
-        <>
-        <div className="w-50 rounded-2xl bg-white border-1 items-center border-gray-200 flex flex-col p-3">
+  return (
+    <>
+      <div className="w-80 rounded-2xl bg-white border-1 items-center border-gray-200 flex p-3">
+        <h1 className="text-6xl font-bold ml-4 mr-7">
+          {propostasAprovadas.length}
+        </h1>
+
+        <div className="flex flex-col justify-center items-start gap-3">
           <h1 className="text-2xl font-bold">Propostas</h1>
-          <div className="flex">
-
-            <div className="flex flex-col justify-center items-center gap-3">
-              Até o momento temos 
-
-              <h1 className="text-4xl font-bold">
-                {propostasAprovadas.length}
-              </h1>
-
-              {propostasAprovadas.length === 0 || null?
-              (<>Até o momento nenhuma Proposta Aprovada</>)
-              : propostasAprovadas.length === 1 ?
+          Até o momento temos
+          {propostasAprovadas.length === 0 || null ?
+            (<>Até o momento nenhuma Proposta Aprovada</>)
+            : propostasAprovadas.length === 1 ?
               (<>Proposta Aprovada</>) : (<>Propostas Aprovadas</>)}
-              
-            </div>
 
-          </div>
         </div>
-        </>
-    )
+      </div>
+    </>
+  )
 }
 
 export default PropostasFechadas
