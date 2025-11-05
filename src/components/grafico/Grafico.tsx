9327fefceb503b6798f3b2e36e305c9e69c91827
nicholasacahu <nikdacap@gmail.com>
2025-11-04 22:54:57 -0300

    Update Grafico.tsx

--- src/components/grafico/Grafico.tsx
+++ src/components/grafico/Grafico.tsx
@@ -4,186 +4,61 @@ import { Area, AreaChart, CartesianGrid, XAxis } from "recharts";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
 import React from "react";
+import { da } from "date-fns/locale";
 
 const url = import.meta.env.VITE_API_URL;
 
-interface todosOsItensDoDia {
-    id: number,
-    idDiarioDeObra: number,
-    idQuantitativa: number,
-    descricao: string,
-    quantidade: number,
-    createdAt: string,
-    updatedAt: string,
-    diarioDeObraItensDia: {
-        id: number,
-        idProposta: number,
-        dataDia: string,
-        createdAt: string,
-        updatedAt: string,
-        idDiarioDeObra: number,
-        idContrato: number
-    }
+interface ChartDataItem {
+    dataDia: string,
+    total_m2: number
 }
 
-// const chartConfig = {
-//     quantidade: {
-//         label: "M²",
-//         color: "#2563eb",
-//     }
-// } satisfies ChartConfig
+interface PeriodoDashBoardPropos {
+    dataInicial: Date | null;
+    dataFinal: Date | null;
+}
 
-function Grafico() {
+function Grafico( { dataInicial, dataFinal }: PeriodoDashBoardPropos) {
 
-    const { data: itensDoDia } = useSuspenseQuery({
-        queryKey: ["itensDoDia"],
+    const { data: chartData } = useSuspenseQuery({
+        queryKey: ["chartData", dataInicial, dataFinal],
         queryFn: async () => {
-            const response = await fetch(`${url}/todosOsItensDoDia`, {
+            const response = await fetch(`${url}/dashboard/${dataInicial}/${dataFinal}`, {
                 method: "GET",
                 credentials: "include"
-            })
-            if (!response.ok) throw new Error("erro ao encontrar propostas");
+            });
+            if (!response.ok) throw new Error("Proposta não encontrada");
             const data = await response.json();
-            return data as todosOsItensDoDia[]
+            return data as ChartDataItem[];
+
         }
     })
 
-    // const { data: itensDaQuantitativa } = useSuspenseQuery({
-    //     queryKey: ["itensDaQuantitativa"],
-    //     queryFn: async () => {
-    //         const response = await fetch(`${url}/quantitativaUnica/${itensDoDia.map((item) => item.idQuantitativa)}`)
-    //         if (!response.ok) throw new Error("erro ao encontrar Quantitativa");
-    //         const data = await response.json();
-    //         return data as todosOsItensDoDia[]
-    //     }
-    // })
-
-// const description = "An interactive area chart"
-
-const chartData = [
-  { date: "2024-04-01", desktop: 222, mobile: 150 },
-  { date: "2024-04-02", desktop: 97, mobile: 180 },
-  { date: "2024-04-03", desktop: 167, mobile: 120 },
-  { date: "2024-04-04", desktop: 242, mobile: 260 },
-  { date: "2024-04-05", desktop: 373, mobile: 290 },
-  { date: "2024-04-06", desktop: 301, mobile: 340 },
-  { date: "2024-04-07", desktop: 245, mobile: 180 },
-  { date: "2024-04-08", desktop: 409, mobile: 320 },
-  { date: "2024-04-09", desktop: 59, mobile: 110 },
-  { date: "2024-04-10", desktop: 261, mobile: 190 },
-  { date: "2024-04-11", desktop: 327, mobile: 350 },
-  { date: "2024-04-12", desktop: 292, mobile: 210 },
-  { date: "2024-04-13", desktop: 342, mobile: 380 },
-  { date: "2024-04-14", desktop: 137, mobile: 220 },
-  { date: "2024-04-15", desktop: 120, mobile: 170 },
-  { date: "2024-04-16", desktop: 138, mobile: 190 },
-  { date: "2024-04-17", desktop: 446, mobile: 360 },
-  { date: "2024-04-18", desktop: 364, mobile: 410 },
-  { date: "2024-04-19", desktop: 243, mobile: 180 },
-  { date: "2024-04-20", desktop: 89, mobile: 150 },
-  { date: "2024-04-21", desktop: 137, mobile: 200 },
-  { date: "2024-04-22", desktop: 224, mobile: 170 },
-  { date: "2024-04-23", desktop: 138, mobile: 230 },
-  { date: "2024-04-24", desktop: 387, mobile: 290 },
-  { date: "2024-04-25", desktop: 215, mobile: 250 },
-  { date: "2024-04-26", desktop: 75, mobile: 130 },
-  { date: "2024-04-27", desktop: 383, mobile: 420 },
-  { date: "2024-04-28", desktop: 122, mobile: 180 },
-  { date: "2024-04-29", desktop: 315, mobile: 240 },
-  { date: "2024-04-30", desktop: 454, mobile: 380 },
-  { date: "2024-05-01", desktop: 165, mobile: 220 },
-  { date: "2024-05-02", desktop: 293, mobile: 310 },
-  { date: "2024-05-03", desktop: 247, mobile: 190 },
-  { date: "2024-05-04", desktop: 385, mobile: 420 },
-  { date: "2024-05-05", desktop: 481, mobile: 390 },
-  { date: "2024-05-06", desktop: 498, mobile: 520 },
-  { date: "2024-05-07", desktop: 388, mobile: 300 },
-  { date: "2024-05-08", desktop: 149, mobile: 210 },
-  { date: "2024-05-09", desktop: 227, mobile: 180 },
-  { date: "2024-05-10", desktop: 293, mobile: 330 },
-  { date: "2024-05-11", desktop: 335, mobile: 270 },
-  { date: "2024-05-12", desktop: 197, mobile: 240 },
-  { date: "2024-05-13", desktop: 197, mobile: 160 },
-  { date: "2024-05-14", desktop: 448, mobile: 490 },
-  { date: "2024-05-15", desktop: 473, mobile: 380 },
-  { date: "2024-05-16", desktop: 338, mobile: 400 },
-  { date: "2024-05-17", desktop: 499, mobile: 420 },
-  { date: "2024-05-18", desktop: 315, mobile: 350 },
-  { date: "2024-05-19", desktop: 235, mobile: 180 },
-  { date: "2024-05-20", desktop: 177, mobile: 230 },
-  { date: "2024-05-21", desktop: 82, mobile: 140 },
-  { date: "2024-05-22", desktop: 81, mobile: 120 },
-  { date: "2024-05-23", desktop: 252, mobile: 290 },
-  { date: "2024-05-24", desktop: 294, mobile: 220 },
-  { date: "2024-05-25", desktop: 201, mobile: 250 },
-  { date: "2024-05-26", desktop: 213, mobile: 170 },
-  { date: "2024-05-27", desktop: 420, mobile: 460 },
-  { date: "2024-05-28", desktop: 233, mobile: 190 },
-  { date: "2024-05-29", desktop: 78, mobile: 130 },
-  { date: "2024-05-30", desktop: 340, mobile: 280 },
-  { date: "2024-05-31", desktop: 178, mobile: 230 },
-  { date: "2024-06-01", desktop: 178, mobile: 200 },
-  { date: "2024-06-02", desktop: 470, mobile: 410 },
-  { date: "2024-06-03", desktop: 103, mobile: 160 },
-  { date: "2024-06-04", desktop: 439, mobile: 380 },
-  { date: "2024-06-05", desktop: 88, mobile: 140 },
-  { date: "2024-06-06", desktop: 294, mobile: 250 },
-  { date: "2024-06-07", desktop: 323, mobile: 370 },
-  { date: "2024-06-08", desktop: 385, mobile: 320 },
-  { date: "2024-06-09", desktop: 438, mobile: 480 },
-  { date: "2024-06-10", desktop: 155, mobile: 200 },
-  { date: "2024-06-11", desktop: 92, mobile: 150 },
-  { date: "2024-06-12", desktop: 492, mobile: 420 },
-  { date: "2024-06-13", desktop: 81, mobile: 130 },
-  { date: "2024-06-14", desktop: 426, mobile: 380 },
-  { date: "2024-06-15", desktop: 307, mobile: 350 },
-  { date: "2024-06-16", desktop: 371, mobile: 310 },
-  { date: "2024-06-17", desktop: 475, mobile: 520 },
-  { date: "2024-06-18", desktop: 107, mobile: 170 },
-  { date: "2024-06-19", desktop: 341, mobile: 290 },
-  { date: "2024-06-20", desktop: 408, mobile: 450 },
-  { date: "2024-06-21", desktop: 169, mobile: 210 },
-  { date: "2024-06-22", desktop: 317, mobile: 270 },
-  { date: "2024-06-23", desktop: 480, mobile: 530 },
-  { date: "2024-06-24", desktop: 132, mobile: 180 },
-  { date: "2024-06-25", desktop: 141, mobile: 190 },
-  { date: "2024-06-26", desktop: 434, mobile: 380 },
-  { date: "2024-06-27", desktop: 448, mobile: 490 },
-  { date: "2024-06-28", desktop: 149, mobile: 200 },
-  { date: "2024-06-29", desktop: 103, mobile: 160 },
-  { date: "2024-06-30", desktop: 446, mobile: 400 },
-]
-
-    console.log(itensDoDia)
     const chartConfig = {
         visitors: {
             label: "Visitors",
         },
-        desktop: {
-            label: "Desktop",
+        M2: {
+            label: "M²",
             color: "var(--chart-1)",
-        },
-        mobile: {
-            label: "Mobile",
-            color: "var(--chart-2)",
-        },
+        }
     } satisfies ChartConfig
 
 
-        const [timeRange, setTimeRange] = React.useState("90d")
-        const filteredData = chartData.filter((item) => {
-            const date = new Date(item.date)
-            const referenceDate = new Date("2024-06-30")
-            let daysToSubtract = 90
-            if (timeRange === "30d") {
-                daysToSubtract = 30
-            } else if (timeRange === "7d") {
-                daysToSubtract = 7
-            }
-            const startDate = new Date(referenceDate)
-            startDate.setDate(startDate.getDate() - daysToSubtract)
-            return date >= startDate
-        })
+    const [timeRange, setTimeRange] = React.useState("90d")
+    // const filteredData = chartData.filter((item) => {
+    //     const date = new Date(item.dataDia)
+    //     const referenceDate = new Date("2024-06-30")
+    //     let daysToSubtract = 90
+    //     if (timeRange === "30d") {
+    //         daysToSubtract = 30
+    //     } else if (timeRange === "7d") {
+    //         daysToSubtract = 7
+    //     }
+    //     const startDate = new Date(referenceDate)
+    //     startDate.setDate(startDate.getDate() - daysToSubtract)
+    //     return date >= startDate
+    // })
 
 
     return (
@@ -220,7 +95,7 @@ const chartData = [
                     config={chartConfig}
                     className="aspect-auto h-[250px] w-full"
                 >
-                    <AreaChart data={filteredData}>
+                    <AreaChart data={chartData}>
                         <defs>
                             <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
                                 <stop
