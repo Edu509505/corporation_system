600a564e2c7bdfe463ff76322cb75a8d0d039a37
nicholasacahu <nikdacap@gmail.com>
2025-11-11 22:26:08 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
 import { CardFaturamento } from "@/components/cardsHome/CardFaturamento";
 import { CardComparacaoPropostas } from "@/components/cardsHome/ComparacaoProposta";
+import { CardPropostasEmAnalise } from "@/components/cardsHome/PropostasEmAnalise";
+import { CardPropostaEmAndamento } from "@/components/cardsHome/PropostasFechadas";
 import Grafico from "@/components/grafico/Grafico";
 
 function Home() {
@@ -8,10 +10,10 @@ function Home() {
       <h1 className="text-xl font-semibold text-gray-800 mb-4">📈 Painel de Propostas</h1>
 
       <div className="flex flex-wrap gap-x-4 gap-y-6">
-        {/* <CardPropostaEmAndamento /> */}
         <CardComparacaoPropostas />
+        <CardPropostaEmAndamento />
+        <CardPropostasEmAnalise/>
         <CardFaturamento/>
-        {/* <CardPropostasEmAnalise/> */}
         {/* Adicione mais cards aqui */}
       </div>
 
